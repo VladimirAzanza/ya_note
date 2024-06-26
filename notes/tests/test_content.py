21e4241a58e9f4d7ec8bc75d59fcb9d7f36b9676
Vladimir Azanza <vladimirazanza@gmail.com>
2024-06-26 13:12:06 +0500

    Reorder the code

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -13,32 +13,38 @@ class TestContentForm(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Peter Pan')
         cls.note = Note.objects.create(
             title='Заголовок', text='Текст', author=cls.author
         )
         cls.auth_author = Client()
         cls.auth_author.force_login(cls.author)
-        cls.add_note_url = reverse('notes:add')
-        cls.note_list_url = reverse('notes:list')
+        cls.auth_reader = Client()
+        cls.auth_reader.force_login(cls.reader)
 
     def test_notes_list_for_different_user(self):
-        reader = User.objects.create(username='Peter Pan')
-        self.client.force_login(reader)
-        available_urls = (
+        for client, note_in_list in (
             (self.auth_author, True),
-            (self.client, False),
-        )
-        for client, note_in_list in available_urls:
+            (self.auth_reader, False),
+        ):
             with self.subTest(client=client, note_in_list=note_in_list):
-                response = client.get(self.note_list_url)
+                url = reverse('notes:list')
+                response = client.get(url)
                 object_list = response.context['object_list']
                 self.assertEqual((self.note in object_list), note_in_list)
 
     def test_auth_client_has_form(self):
-        response = self.auth_author.get(self.add_note_url)
-        self.assertIn('form', response.context)
-        self.assertIsInstance(response.context['form'], NoteForm)
+        for name, args in (
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,))
+        ):
+            with self.subTest(name=name, args=args):
+                url = reverse(name, args=args)
+                response = self.auth_author.get(url)
+                self.assertIn('form', response.context)
+                self.assertIsInstance(response.context['form'], NoteForm)
 
     def test_anonymous_client_has_no_form(self):
-        response = self.client.get(self.add_note_url)
+        url = reverse('notes:add')
+        response = self.client.get(url)
         self.assertIsNone(response.context)
